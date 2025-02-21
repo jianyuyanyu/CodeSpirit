d3b7571b27be94a548b88d19e212c9f207f28a15
李文强 <codelove1314@live.cn>
2025-02-21 11:41:05 +0800

    修改引用

--- Src/CodeSpirit.AppHost/Program.cs
+++ Src/CodeSpirit.AppHost/Program.cs
@@ -1,11 +1,9 @@
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache")
-                   .WithRedisInsight()
+                   .WithLifetime(ContainerLifetime.Persistent)
                    .WithRedisCommander()
                    .WithDataVolume(isReadOnly: false);
-
-//IResourceBuilder<ProjectResource> apiService = builder.AddProject<Projects.CodeSpirit_ApiService>("apiservice");
 
 // Add Seq logging service
 var seqService = builder.AddSeq("seq")
@@ -16,15 +14,18 @@ var seqService = builder.AddSeq("seq")
 
 builder.AddProject<Projects.CodeSpirit_IdentityApi>("identity-api")
     .WithReference(seqService)
-    .WithReference(cache);
+    .WithReference(cache)
+    ;
 
 // 添加 ConfigCenter 服务
 builder.AddProject<Projects.CodeSpirit_ConfigCenter>("config-api")
     .WithReference(seqService)
-    .WithReference(cache);
+    .WithReference(cache)
+    ;
 
 builder.AddProject<Projects.CodeSpirit_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithReference(seqService);
+    .WithReference(seqService)
+    ;
 
 builder.Build().Run();

01e8e169ef0286f942025b454e44c8a00da7b888
李文强 <codelove1314@live.cn>
2025-08-15 11:23:33 +0800

    修改导入文本

--- Src/CodeSpirit.ExamApi/Dtos/Question/QuestionImportFromTextDto.cs
+++ Src/CodeSpirit.ExamApi/Dtos/Question/QuestionImportFromTextDto.cs
@@ -24,7 +24,7 @@ namespace CodeSpirit.ExamApi.Dtos.Question
         /// Word格式文本
         /// </summary>
         [DisplayName("Word格式文本")]
-        [AmisFormField(type: "editor",Placeholder = @"一、单项选择题（每题1分，80小题，共计80分）
+        [AmisFormField(type: "editor", Placeholder = @"一、单项选择题（每题1分，80小题，共计80分）
 1、SSL协议最早是由(B)提出的。
 A、Microsoft
 B、Netscape
@@ -33,13 +33,21 @@ D、IBM
 【难度】困难
 【解析】SSL协议是由Netscape公司提出的，用于在互联网上提供安全通信。
 【标签】SSL、安全通信
-
-二、判断题（每空1分，共计20分）
+二、多项选择题（每题2分，10小题，共计20分）
+1、以下哪些是常用的编程语言？(ABC)
+A、Java
+B、Python
+C、C#
+D、HTML
+【难度】中等
+【解析】Java、Python、C#都是编程语言，而HTML是标记语言。
+【标签】编程语言、技术
+三、判断题（每空1分，共计20分）
 1. 平邮包裹的到货周期较长，顾客通常要7 - 15天才能收到购买的商品, 但是提供了网上查询物流进程的服务。（ 对 ）
 【难度】困难
 【解析】平邮包裹确实有网上查询物流进程的服务。
 【标签】平邮、商品
-", AdditionalConfig = "{\"language\":\"markdown\",\"size\":\"xl\"}")]
+", AdditionalConfig = "{\"language\":\"markdown\",\"size\":\"xxl\"}")]
         [Required]
         public required string Text { get; set; }
     }
